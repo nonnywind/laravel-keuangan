47a361b61aa429c420071c420e902828152b7fb1
nonnywind <nonnywindarti@gmail.com>
2020-07-20 03:00:43 +0700

    Authorization

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
+
+Route::get('add-user', function () {
+    \DB::table('users')->insert([
+        'name' => 'admin',
+        'email' => 'noni@gmail.com',
+        'password' => bcrypt('123'),
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_At' => date('Y-m-d H:i:s')
+    ]);
+});
+
+Auth::routes();
+
+Route::get('/home', function () {
+    return redirect('/');
 });
