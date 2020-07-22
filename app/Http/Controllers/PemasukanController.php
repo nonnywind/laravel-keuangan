7847fcc2e0fd90ef8a79b11864146b6b0cfb81d6
nonnywind <nonnywindarti@gmail.com>
2020-07-22 23:08:50 +0700

    Read data pemasukan (client side)

--- app/Http/Controllers/PemasukanController.php
+++ app/Http/Controllers/PemasukanController.php
@@ -8,6 +8,8 @@ class PemasukanController extends Controller
 {
     public function index()
     {
-        return view('pemasukan.pemasukan_index');
+        $data = \DB::table('pemasukan as a')->join('sumber as b', 'a.sumber_pemasukan', '=', 'b.id')->get();
+
+        return view('pemasukan.pemasukan_index', compact('data'));
     }
 }
--- resources/views/pemasukan/pemasukan_index.blade.php
+++ resources/views/pemasukan/pemasukan_index.blade.php
@@ -1,1 +1,66 @@
 @extends('layouts.master')
+
+@section('content')
+
+<div class="row">
+        <div class="col">
+          <div class="card">
+            <!-- Card header -->
+            <div class="card-header border-0">
+              <h3 class="mb-0">Data Pemasukan</h3>
+            </div>
+            <!-- Light table -->
+            <div class="table-responsive">
+              <table class="table align-items-center table-flush">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col" class="sort" data-sort="name">#</th>
+                    <th scope="col" class="sort" data-sort="budget">Sumber</th>
+                    <th scope="col" class="sort" data-sort="status">Nominal</th>
+                    <th scope="col" class="sort" data-sort="completion">Tanggal</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody class="list">
+                @foreach ($data as $index=>$dt)
+                  <tr>
+                    <td>{{$index+1}}</td>
+                    <td>{{$dt->nama}}</td>
+                    <td>{{$dt->nominal}}</td>
+                    <td>{{date('d M Y', strtotime($dt->tanggal))}}</td>
+                  </tr>
+                @endforeach
+                </tbody>
+              </table>
+            </div>
+            <!-- Card footer -->
+            <div class="card-footer py-4">
+              <nav aria-label="...">
+                <ul class="pagination justify-content-end mb-0">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">
+                      <i class="fas fa-angle-left"></i>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                  </li>
+                  <li class="page-item active">
+                    <a class="page-link" href="#">1</a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">
+                      <i class="fas fa-angle-right"></i>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+@endsection
